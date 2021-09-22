0d0c7f908d620370e8e39ac008dbb0a531b70a41
huchi57 <felixhu0312@gmail.com>
2021-09-22 17:44:22 +0800

    Create ObjectsPreWarmer.cs

---
+++ ObjectsPreWarmer.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-15)]
+public class ObjectsPreWarmer : MonoBehaviour
+{
+    [System.Serializable]
+    public enum PreWarmTimeStepInterval
+    {
+        [InspectorName("Use Time.deltaTime")]
+        UseDeltaTime,
+        UseCustomTimeInterval
+    }
+
+    [SerializeField]
+    private bool autoWarmingOnAwake = true;
+
+    [SerializeField]
+    private int objectsToAwakePerOperation = 1;
+
+    [Header("Time Interval Between Awake Operations")]
+
+    [SerializeField]
+    private PreWarmTimeStepInterval timeStepIntervalMode;
+
+    [SerializeField]
+    private float customTimeStepInterval = 0.01f;
+
+    [Space(10)]
+
+    [SerializeField]
+    private GameObject[] preWarmObjects;
+
+    private WaitForSeconds wait;
+    private bool[] cacheObjectActiveStates;
+
+    private void Awake()
+    {
+        HideObjects();
+        if (autoWarmingOnAwake)
+        {
+            WarmObjects();
+        }
+    }
+
+    public void WarmObjects()
+    {
+        if (preWarmObjects != null)
+        {
+            StartCoroutine(DoWarm());
+        }
+    }
+
+    IEnumerator DoWarm()
+    {
+        wait = new WaitForSeconds(customTimeStepInterval);
+        for (int i = 0; i < preWarmObjects.Length; i++)
+        {
+            yield return timeStepIntervalMode == PreWarmTimeStepInterval.UseDeltaTime ? null : wait;
+            if (preWarmObjects[i] != null)
+            {
+                WarmObjectsFromIndex(i);
+                i += objectsToAwakePerOperation - 1;
+            }
+        }
+    }
+
+    private void HideObjects()
+    {
+        if (preWarmObjects == null)
+        {
+            return;
+        }
+        cacheObjectActiveStates = new bool[preWarmObjects.Length];
+        for (int i = 0; i < preWarmObjects.Length; i++)
+        {
+            if (preWarmObjects[i] != null)
+            {
+                cacheObjectActiveStates[i] = preWarmObjects[i].activeSelf;
+                preWarmObjects[i].SetActive(false);
+            }
+        }
+    }
+
+    private void WarmObjectsFromIndex(int currentIndex)
+    {
+        for (int i = currentIndex; i < currentIndex + objectsToAwakePerOperation; i++)
+        {
+            if (i >= preWarmObjects.Length) 
+            {
+                break;
+            }
+            if (preWarmObjects[i] != null)
+            {
+                preWarmObjects[i].SetActive(cacheObjectActiveStates[i]);
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (objectsToAwakePerOperation < 1)
+        {
+            objectsToAwakePerOperation = 1;
+        }
+        if (customTimeStepInterval < 0)
+        {
+            customTimeStepInterval = 0;
+        }
+    }
+
+}
